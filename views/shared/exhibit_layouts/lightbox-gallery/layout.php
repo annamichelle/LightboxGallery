5a74cb464070d775c625a96a9fd6fe150dce4dc9
Anna Michelle <anna_michelle@msn.com>
2014-11-17 16:22:50 -0600

    added lightbox attributes to gallery thumbnails
    still working on changing where they link to

--- views/shared/exhibit_layouts/lightbox-gallery/layout.php
+++ views/shared/exhibit_layouts/lightbox-gallery/layout.php
@@ -11,11 +11,24 @@ $galleryPosition = isset($options['gallery-position'])
 <div class="gallery-showcase <?php echo $showcasePosition; ?> with-<?php echo $galleryPosition; ?>">
     <?php
         $attachment = array_shift($attachments);
-        echo $this->exhibitAttachment($attachment, array('imageSize' => 'fullsize'));
+        echo $this->exhibitAttachment($attachment, 
+        	array('imageSize' => 'fullsize', 
+        		'linkToFile' => 'true', 
+        		'linkToMetadata' => 'false', 
+        		'imgAttributes' => array('data-lightbox' => 'lightbox-gallery')
+        	)
+        );
     ?>
 </div>
 <?php endif; ?>
 <div class="gallery <?php if ($showcaseFile || !empty($text)) echo "with-showcase $galleryPosition"; ?>">
-    <?php echo $this->exhibitAttachmentGallery($attachments); ?>
+    <?php 
+    	echo $this->exhibitAttachmentGallery($attachments,
+    		array('linkToMetadata' => 'false',
+    			'linkToFile' => 'fullsize',
+        		'imgAttributes' => array('data-lightbox' => 'lightbox-gallery')
+        	)
+    	); 
+    ?>
 </div>
 <?php echo $text; ?>
