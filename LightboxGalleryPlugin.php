b5597fd5e0b4eae8a51c61fe7ce77e51e2a8ee70
Anna Michelle <anna_michelle@msn.com>
2014-12-05 10:45:01 -0600

    rearranged layout order in exhibit builder
    to reflect order of standard layouts

--- LightboxGalleryPlugin.php
+++ LightboxGalleryPlugin.php
@@ -21,13 +21,13 @@ class LightboxGalleryPlugin extends Omeka_Plugin_AbstractPlugin
 	}
 
 	public function filterExhibitLayouts($layouts) {
-    	$layouts['lightbox-gallery'] = array(
-        	'name' => 'Lightbox Gallery',
-        	'description' => 'A gallery layout that uses Lightbox to display all files attached to an exhibit page.'
-    	);
     	$layouts['lightbox-file-text'] = array(
         	'name' => 'Lightbox File with Text',
         	'description' => 'A layout that features files justified to the left or right and uses Lightbox to display larger versions of those files.'
+    	);
+    	$layouts['lightbox-gallery'] = array(
+        	'name' => 'Lightbox Gallery',
+        	'description' => 'A gallery layout that uses Lightbox to display all files attached to an exhibit page.'
     	);
     return $layouts;
 	}
